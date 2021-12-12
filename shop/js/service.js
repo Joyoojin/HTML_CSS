8358db9b9c8be06102f3f2e896ab8f6778aff3ae
Joyoojin <90246581+Joyoojin@users.noreply.github.com>
2021-12-12 18:55:59 +0900

    12.12

---
+++ shop/js/service.js
@@ -0,0 +1,10 @@
+/*tawk 상담 */
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/617d458086aee40a57391c09/1fj8lf0a0';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
